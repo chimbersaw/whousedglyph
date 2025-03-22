10fe68da40f2f882131627190c61c9ad79bc72a7
Maksim Sukhodolskii <maksukh@inbox.ru>
2025-03-22 05:30:55 +0100

    Add allowed host to .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,23 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    preview: {
-        port: 4173,
-        strictPort: true,
-        host: 'localhost',
-        allowedHosts: ['whousedglyph.com', 'www.whousedglyph.com']
-    },
-    server: {
-        port: 5173,
-        strictPort: true,
-        host: 'localhost',
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    const host = env.VITE_ALLOWED_HOST || "localhost";
+
+    return {
+        plugins: [react()],
+        preview: {
+            port: 4173,
+            strictPort: true,
+            host: 'localhost',
+            allowedHosts: [host, `www.${host}`]
+        },
+        server: {
+            port: 5173,
+            strictPort: true,
+            host: 'localhost',
+        }
     }
 });
