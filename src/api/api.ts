288b8a1f23ec93f689cee0964d4fad55496a2060
Maksim Sukhodolskii <maksukh@inbox.ru>
2025-03-22 04:37:08 +0100

    Add protocol to env

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+const protocol= import.meta.env.VITE_API_PROTOCOL || "http";
 const host = import.meta.env.VITE_API_HOST || "127.0.0.1";
 const port = import.meta.env.VITE_API_PORT || "8000";
 
 export const instance = axios.create({
-    baseURL: `http://${host}:${port}/api`
+    baseURL: `${protocol}://${host}:${port}/api`
 });
